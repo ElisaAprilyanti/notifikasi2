7e0ba78c2fdd3f08b69ef4ef386b2502b68f5b78
elisaaprilyanti <elisaaprilyanti04@gmail.com>
2019-04-03 00:00:20 +0700

    first commit

---
+++ FootBallPlayer/app/src/main/java/com/apahipeh/myunit/footballplayer/ListViewAdapter.java
@@ -0,0 +1,10 @@
+package com.apahipeh.myunit.footballplayer;
+
+import android.content.Context;
+
+import java.util.List;
+
+class ListViewAdapter {
+    public ListViewAdapter(List<PlayerItem> playerItemList, Context applicationContext) {
+    }
+}
---
+++ FootBallPlayer/app/src/main/java/com/apahipeh/myunit/footballplayer/PlayerItem.java
@@ -0,0 +1,6 @@
+package com.apahipeh.myunit.footballplayer;
+
+class PlayerItem {
+    public PlayerItem(String no, String name, String position, String birth_date, String poster) {
+    }
+}
